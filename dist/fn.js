905a45d21d1f1d89ee51677d528fa578928def03
Brandon Garcia <bcgarcia44@gmail.com>
2019-05-19 15:52:42 -0500

    greatly simplifying library for ease of import

--- dist/fn.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ifElse = (expr, onTrue, onFalse) => expr ? onTrue() : onFalse();
-const compose = (first, second) => (param) => second(first(param));
-exports.FnUtils = {
-    compose,
-    ifElse,
-};
-//# sourceMappingURL=fn.js.map
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./fn";
-export * from "./optional";
-export * from "./pipeline";
-export * from "./types";
-//# sourceMappingURL=index.d.ts.map
