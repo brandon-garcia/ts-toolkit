8389daff4b6350cbdd3a16e9b77243be57d0ffef
Brandon Garcia <bcgarcia44@gmail.com>
2019-06-15 05:15:42 -0500

    expanding function types and applicator factory methods

--- src/applicator.ts
+++ src/applicator.ts
@@ -1,4 +1,4 @@
-import {Fn, Fn2, Fn3, Fn4, Supplier} from "./fn";
+import {Fn, Fn2, Fn3, Fn4, Fn5, Fn6, Fn7, Fn8, Supplier} from "./fn";
 
 interface IApplicator0<R> {
   toCallable(): Supplier<R>;
@@ -28,7 +28,42 @@ interface IApplicator4<T1, T2, T3, T4, R> {
   toCallable(): Fn4<T1, T2, T3, T4, R>;
 }
 
+interface IApplicator4<T1, T2, T3, T4, R> {
+  left(param: T1): IApplicator3<T2, T3, T4, R>;
+  right(param: T4): IApplicator3<T1, T2, T3, R>;
+  toCallable(): Fn4<T1, T2, T3, T4, R>;
+}
+
+interface IApplicator5<T1, T2, T3, T4, T5, R> {
+  left(param: T1): IApplicator4<T2, T3, T4, T5, R>;
+  right(param: T4): IApplicator4<T1, T2, T3, T4, R>;
+  toCallable(): Fn5<T1, T2, T3, T4, T5, R>;
+}
+
+interface IApplicator6<T1, T2, T3, T4, T5, T6, R> {
+  left(param: T1): IApplicator5<T2, T3, T4, T5, T6, R>;
+  right(param: T4): IApplicator5<T1, T2, T3, T4, T5, R>;
+  toCallable(): Fn6<T1, T2, T3, T4, T5, T6, R>;
+}
+
+interface IApplicator7<T1, T2, T3, T4, T5, T6, T7, R> {
+  left(param: T1): IApplicator6<T2, T3, T4, T5, T6, T7, R>;
+  right(param: T4): IApplicator6<T1, T2, T3, T4, T5, T6, R>;
+  toCallable(): Fn7<T1, T2, T3, T4, T5, T6, T7, R>;
+}
+
+interface IApplicator8<T1, T2, T3, T4, T5, T6, T7, T8, R> {
+  left(param: T1): IApplicator7<T2, T3, T4, T5, T6, T7, T8, R>;
+  right(param: T4): IApplicator7<T1, T2, T3, T4, T5, T6, T7, R>;
+  toCallable(): Fn8<T1, T2, T3, T4, T5, T6, T7, T8, R>;
+}
+
 export class Applicator {
+
+  public static from0<R>(fn: Supplier<R>): IApplicator0<R> {
+    return new Applicator(fn) as IApplicator0<R>;
+  }
+
   public static from1<T1, R>(fn: Fn<T1, R>): IApplicator1<T1, R> {
     return new Applicator(fn) as IApplicator1<T1, R>;
 
@@ -46,6 +81,21 @@ export class Applicator {
     return new Applicator(fn) as IApplicator4<T1, T2, T3, T4, R>;
   }
 
+  public static from5<T1, T2, T3, T4, T5, R>(fn: Fn5<T1, T2, T3, T4, T5, R>): IApplicator5<T1, T2, T3, T4, T5, R> {
+    return new Applicator(fn) as IApplicator5<T1, T2, T3, T4, T5, R>;
+  }
+
+  public static from6<T1, T2, T3, T4, T5, T6, R>(fn: Fn6<T1, T2, T3, T4, T5, T6, R>): IApplicator6<T1, T2, T3, T4, T5, T6, R> {
+    return new Applicator(fn) as IApplicator6<T1, T2, T3, T4, T5, T6, R>;
+  }
+
+  public static from7<T1, T2, T3, T4, T5, T6, T7, R>(fn: Fn7<T1, T2, T3, T4, T5, T6, T7, R>): IApplicator7<T1, T2, T3, T4, T5, T6, T7, R> {
+    return new Applicator(fn) as IApplicator7<T1, T2, T3, T4, T5, T6, T7, R>;
+  }
+
+  public static from8<T1, T2, T3, T4, T5, T6, T7, T8, R>(fn: Fn8<T1, T2, T3, T4, T5, T6, T7, T8, R>): IApplicator8<T1, T2, T3, T4, T5, T6, T7, T8, R> {
+    return new Applicator(fn) as IApplicator8<T1, T2, T3, T4, T5, T6, T7, T8, R>;
+  }
   private constructor(private readonly fn: (...args: any[]) => any) {
   }
 
--- src/fn.ts
+++ src/fn.ts
@@ -3,12 +3,20 @@ export type Fn<P1, R> = (p1: P1) => R;
 export type Fn2<P1, P2, R> = (p1: P1, p2: P2) => R;
 export type Fn3<P1, P2, P3, R> = (p1: P1, p2: P2, p3: P3) => R;
 export type Fn4<P1, P2, P3, P4, R> = (p1: P1, p2: P2, p3: P3, p4: P4) => R;
+export type Fn5<P1, P2, P3, P4, P5, R> = (p1: P1, p2: P2, p3: P3, p4: P4, p5: P5) => R;
+export type Fn6<P1, P2, P3, P4, P5, P6, R> = (p1: P1, p2: P2, p3: P3, p4: P4, p5: P5, p6: P6) => R;
+export type Fn7<P1, P2, P3, P4, P5, P6, P7, R> = (p1: P1, p2: P2, p3: P3, p4: P4, p5: P5, p6: P6, p7: P7) => R;
+export type Fn8<P1, P2, P3, P4, P5, P6, P7, P8, R> = (p1: P1, p2: P2, p3: P3, p4: P4, p5: P5, p6: P6, p7: P7, p8: P8) => R;
 
 export type AsyncFn0<R> = Fn0<Promise<R>>;
 export type AsyncFn<P1, R> = Fn<P1, Promise<R>>;
 export type AsyncFn2<P1, P2, R> = Fn2<P1, P2, Promise<R>>;
 export type AsyncFn3<P1, P2, P3, R> = Fn3<P1, P2, P3, Promise<R>>;
 export type AsyncFn4<P1, P2, P3, P4, R> = Fn4<P1, P2, P3, P4, Promise<R>>;
+export type AsyncFn5<P1, P2, P3, P4, P5, R> = Fn5<P1, P2, P3, P4, P5, Promise<R>>;
+export type AsyncFn6<P1, P2, P3, P4, P5, P6, R> = Fn6<P1, P2, P3, P4, P5, P6, Promise<R>>;
+export type AsyncFn7<P1, P2, P3, P4, P5, P6, P7, R> = Fn7<P1, P2, P3, P4, P5, P6, P7, Promise<R>>;
+export type AsyncFn8<P1, P2, P3, P4, P5, P6, P7, P8, R> = Fn8<P1, P2, P3, P4, P5, P6, P7, P8, Promise<R>>;
 
 export type Consumer<T> = Fn<T, void>;
 export type Supplier<T> = Fn0<T>;
