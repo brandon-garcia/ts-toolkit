873210c61ff15acd0e74cfaacd620a510b1b488f
Brandon Garcia <bcgarcia44@gmail.com>
2019-07-07 23:39:15 -0500

    strings: added isAlphaNumeric

--- src/strings.ts
+++ src/strings.ts
@@ -1,13 +1,18 @@
-
 const regexNumeric = /^\d+$/.compile();
+const regexAlphaNumeric = /^[0-9a-zA-Z]+$/.compile();
 
 const getSplitter = (delim: string) =>
   (str: string) => str.split(delim);
 
 const trim = (str: string) => str.trim();
 
+const isNumeric = (str: string) => str.length > 0 && regexNumeric.test(str);
+
+const isAlphaNumeric = (str: string) => str.length > 0 && regexAlphaNumeric.test(str);
+
 export const StringUtils = {
-  isNumeric: (str: string) => str.length > 0 && regexNumeric.test(str),
+  isAlphaNumeric,
+  isNumeric,
   stripWhitespace: (str: string) => str.replace(/\s/g, ""),
   stripNonNumeric: (str: string) => str.replace(/\D/g, ""),
   split: getSplitter,
