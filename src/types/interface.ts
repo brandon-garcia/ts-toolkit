d42878385a418e344e3298cb80df9dbe50cae49e
Brandon Garcia <bcgarcia44@gmail.com>
2019-07-10 00:07:26 -0500

    types: added Nullable and cleaned up other helper types

--- src/types/interface.ts
+++ src/types/interface.ts
@@ -1,8 +1,9 @@
 
-export type TypeGuard<KnownT, MaybeT extends KnownT> = (p: KnownT) => p is MaybeT;
-export type TypeGuardType<TG extends TypeGuard<unknown, unknown>> = TG extends TypeGuard<unknown, infer T> ? T : never;
+export type TypeGuard<KnownT = unknown, MaybeT extends KnownT = KnownT> = (p: KnownT) => p is MaybeT;
+export type TypeGuardType<TG extends TypeGuard> = TG extends TypeGuard<unknown, infer T> ? T : never;
 
-export type NonNull<T> = T extends null | undefined ? Exclude<T, null | undefined> : T;
+export type NonNull<T = unknown> = Exclude<T, null | undefined>;
+export type Nullable<T extends NonNull = NonNull> = T | null | undefined;
 
 export type Fields<T> = {
   [K in keyof T]: T[K] extends Function ? never : T[K];
